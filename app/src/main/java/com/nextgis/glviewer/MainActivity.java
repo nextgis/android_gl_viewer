b16e7294c4c74f72667865ad096958ea6f90758b
NikitaFeodonit <nikitafeodonit@yandex.com>
2016-12-08 19:16:30 +0300

    Light and dark themes

--- app/src/main/java/com/nextgis/glviewer/MainActivity.java
+++ app/src/main/java/com/nextgis/glviewer/MainActivity.java
@@ -1,23 +1,27 @@
 package com.nextgis.glviewer;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.nextgis.libngui.activity.NGActivity;
 
 
 public class MainActivity
-        extends AppCompatActivity
+        extends NGActivity
 {
+    protected Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
     }
 
 
